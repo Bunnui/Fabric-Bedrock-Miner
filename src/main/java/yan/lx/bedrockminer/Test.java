35c1f792b3ea5f8151335bce8864c2211277d522
bunnyi116 <1160706050@qq.com>
2025-01-01 11:15:31 +0800

    update

--- src/main/java/yan/lx/bedrockminer/Test.java
+++ src/main/java/yan/lx/bedrockminer/Test.java
@@ -12,6 +12,7 @@ import yan.lx.bedrockminer.utils.BlockPlacerUtils;
 
 import static net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.argument;
 import static net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.literal;
+import static yan.lx.bedrockminer.BedrockMiner.*;
 
 public class Test {
 
@@ -21,17 +22,9 @@ public class Test {
     }
 
     public static int executes(CommandContext<FabricClientCommandSource> context) {
-        var client = MinecraftClient.getInstance();
-        var world = client.world;
-        var player = client.player;
-        var hitResult = client.crosshairTarget;
-        var interactionManager = client.interactionManager;
-        var networkHandler = client.getNetworkHandler();
-        if (world == null || player == null || hitResult == null || networkHandler == null)
-            return 0;
-        // 方块选中
-        if (hitResult.getType() == HitResult.Type.BLOCK) {
-            var blockHitResult = (BlockHitResult) hitResult;
+        // 获取玩家瞄准的方块
+        if (crosshairTarget != null && crosshairTarget.getType() == HitResult.Type.BLOCK) {
+            var blockHitResult = (BlockHitResult) crosshairTarget;
             var blockPos = blockHitResult.getBlockPos();
             var blockState = world.getBlockState(blockPos);
             var block = blockState.getBlock();
@@ -41,7 +34,6 @@ public class Test {
 
     private static int executesBlockPos(CommandContext<FabricClientCommandSource> context) {
         var blockPos = BlockPosArgumentType.getBlockPos(context, "blockPos");
-        BlockPlacerUtils.placement(blockPos.up(), Direction.NORTH);
         return 0;
     }
 }
